faebf3c6af79375c1e739b3158224fb2304fb913
Basudev-Das25 <basudevd983@gmail.com>
2025-12-28 20:33:10 +0530

    Added train-test split to prevent Overfitting

--- model/train.py
+++ model/train.py
@@ -1,13 +1,22 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 #load dataset
 data=pd.read_csv("../data/students.csv")
 
-print("Dataset preview:")
-print(data.head())
+#separate features
+X=data[["hours_studied", "attendance", "previous_score"]]
+Y=data["final_score"]
 
-print("\nDataset information:")
-print(data.info())
+#Spli data into testing and training sets
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X,
+    Y,
+    test_size=0.2,
+    random_state=42
+)
 
-print("\nStatistical summary:")
-print(data.describe())
+print("Training feature shape:", X_train.shape)
+print("Testing feature shape:", X_test.shape)
+print("Training feature shape:", Y_train.shape)
+print("Testing feature shape:", Y_test.shape)
