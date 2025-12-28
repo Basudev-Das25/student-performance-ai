68e390469dcbcb8ab4e302641460d388be26a606
Basudev-Das25 <basudevd983@gmail.com>
2025-12-28 23:33:07 +0530

    Trained and saved ML model for deployment

--- model/train.py
+++ model/train.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
@@ -28,18 +29,23 @@ Y_pred=model.predict(X_test)
 #Evaluation
 mse= mean_squared_error(Y_test, Y_pred)
 
-print("Model training completed.")
-
+print("Model trained successfully.")
 print("Mean Squared Error (MSE):", mse)
 
-print("\nActual vs Predicted:")
-for actual, predicted in zip(Y_test, Y_pred):
-    print(f"Actual: {actual}, Predicted: {round(predicted, 2)}")
+# print("\nActual vs Predicted:")
+# for actual, predicted in zip(Y_test, Y_pred):
+#     print(f"Actual: {actual}, Predicted: {round(predicted, 2)}")
 
-sample_input=pd.DataFrame(
-    [[6, 85, 75]],
-    columns=["hours_studied", "attendance", "previous_score"]
-)
-sample_predicition= model.predict(sample_input)
+# sample_input=pd.DataFrame(
+#     [[6, 85, 75]],
+#     columns=["hours_studied", "attendance", "previous_score"]
+# )
+# sample_predicition= model.predict(sample_input)
 
-print("\nSample Prediction for input [6, 85, 75]:", round(sample_predicition[0], 2))
+# print("\nSample Prediction for input [6, 85, 75]:", round(sample_predicition[0], 2))
+
+#save the trained model
+with open("../app/model.pkl", "wb") as f:
+    pickle.dump(model, f)
+
+print("Model saved as model.pkl")
