31b16be18fb2803f35eea1715bced76e3dd9b343
Basudev-Das25 <basudevd983@gmail.com>
2025-12-28 23:22:47 +0530

    Evaluated model using MSE and tested prediction

--- model/train.py
+++ model/train.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 
 #load dataset
 data=pd.read_csv("../data/students.csv")
@@ -21,10 +22,24 @@ X_train, X_test, Y_train, Y_test = train_test_split(
 model = LinearRegression()
 model.fit(X_train, Y_train)
 
+#Predict on test data
+Y_pred=model.predict(X_test)
+
+#Evaluation
+mse= mean_squared_error(Y_test, Y_pred)
+
 print("Model training completed.")
 
-print("\nLearned Weights:")
-for feature, coef in zip(X.columns, model.coef_):
-    print(f"{feature}: {coef}")
+print("Mean Squared Error (MSE):", mse)
 
-print("\nBias (Intercept):", model.intercept_)
+print("\nActual vs Predicted:")
+for actual, predicted in zip(Y_test, Y_pred):
+    print(f"Actual: {actual}, Predicted: {round(predicted, 2)}")
+
+sample_input=pd.DataFrame(
+    [[6, 85, 75]],
+    columns=["hours_studied", "attendance", "previous_score"]
+)
+sample_predicition= model.predict(sample_input)
+
+print("\nSample Prediction for input [6, 85, 75]:", round(sample_predicition[0], 2))
