32b1b16e70fa7f5a93a917c9b93b1b8608338855
Basudev-Das25 <basudevd983@gmail.com>
2025-12-28 23:00:28 +0530

    Trained linear regression model and inspected weights

--- model/train.py
+++ model/train.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 #load dataset
 data=pd.read_csv("../data/students.csv")
@@ -16,7 +17,14 @@ X_train, X_test, Y_train, Y_test = train_test_split(
     random_state=42
 )
 
-print("Training feature shape:", X_train.shape)
-print("Testing feature shape:", X_test.shape)
-print("Training feature shape:", Y_train.shape)
-print("Testing feature shape:", Y_test.shape)
+#Create and Train the model
+model = LinearRegression()
+model.fit(X_train, Y_train)
+
+print("Model training completed.")
+
+print("\nLearned Weights:")
+for feature, coef in zip(X.columns, model.coef_):
+    print(f"{feature}: {coef}")
+
+print("\nBias (Intercept):", model.intercept_)
